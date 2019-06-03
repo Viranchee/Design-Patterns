40a2fbc512a928e6931756338cb28cb6134feefd
Viranchee <viranchee@heady.io>
2019-06-03 12:34:02 +0530

    Facade Pattern

---
+++ Design Patterns.playground/Sources/1_Facade.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+/// Functions
+public func square(number: Float) -> Float {
+    return number * number
+}
+
+public func pi() -> Float {
+    return 3.141592653
+}
+
+public func increment(number: Int) -> Int {
+    return number + 1
+}
+
+public func radius_From(diameter: Float) -> Float {
+    return diameter / 2
+}
+
+// SpawnFacade
