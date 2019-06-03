ef423c728341ec26fb8f247ea8d66264fde5e55e
Viranchee <viranchee@heady.io>
2019-06-03 15:09:47 +0530

    Interface Objects updated

--- Singleton, Delegate.playground/Contents.swift
+++ Singleton, Delegate.playground/Contents.swift
@@ -23,7 +23,5 @@ class MyViewController : UIViewController {
 
 
 
-
-
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()
--- Singleton, Delegate.playground/Sources/Interface.swift
+++ Singleton, Delegate.playground/Sources/Interface.swift
@@ -14,13 +14,12 @@ public protocol WeatherProvider {
     mutating func update_City(new_City: String)
 }
 
-public struct Weather: WeatherProvider {
-    
+public struct YahooWeather: WeatherProvider {
     
     public private(set) var city: String
     public var temprature: Int {
         get {
-            return MockDataProvider().mockTemp
+            return MockDataProvider().mockTemp()
         }
     }
     
@@ -38,15 +37,47 @@ public struct Weather: WeatherProvider {
 }
 
 public struct MockDataProvider {
-    public var mockTemp = Temperature.random(in: 8...40)
+    
+    let minTemperature: Temperature
+    let maxTemperature: Temperature
+    
+    public func mockTemp() -> Temperature {
+        return Temperature.random(in: minTemperature...maxTemperature)
+    }
+    public init(min: Temperature = 8, max: Temperature = 40) {
+        self.minTemperature = min
+        self.maxTemperature = max
+    }
     
     public func mockForecast(days: Int) -> [Temperature] {
         
         var data: [Temperature] = []
         for _ in 0...days {
-            data.append(mockTemp)
+            data.append(mockTemp())
         }
         return data
     }
     
+}
+
+public struct OpenWeather: WeatherProvider {
+    
+    public private(set) var city: String
+    public var temprature: Int {
+        get {
+            return MockDataProvider(min: -20, max: 5).mockTemp()
+        }
+    }
+    
+    public init(city: String) {
+        self.city = city
+    }
+    
+    public func get_Forecast(for_Days: Int) -> [Temperature] {
+        return MockDataProvider(min: -20, max: 5).mockForecast(days: for_Days)
+    }
+    
+    public mutating func update_City(new_City: String) {
+        city = new_City
+    }
 }
